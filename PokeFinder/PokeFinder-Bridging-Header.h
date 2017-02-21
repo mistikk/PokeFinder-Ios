32682087ae500d80dba2828ddacd7d7947328944
mistikk <mustafa-buyukcelebi@hotmail.com>
2017-02-21 21:23:00 +0300

    finished

---
+++ PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Mustafa Buyukcelebi on 11.02.2017.
+//  Copyright © 2017 Mustafa Buyukcelebi. All rights reserved.
+//
+
+#import "GeoFire.h"
